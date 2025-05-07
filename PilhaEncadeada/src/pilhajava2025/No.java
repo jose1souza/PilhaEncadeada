a43de023263b4053bde8913895b11f4bdb5ba292
José Carlos Souza <166337638+jose1souza@users.noreply.github.com>
2025-05-07 08:38:18 -0300

    Add files via upload

---
+++ PilhaEncadeada/src/pilhajava2025/No.java
@@ -0,0 +1,27 @@
+package pilhajava2025;
+
+public class No<T> {
+    private T dado;
+    private No<T> proximo; // referencia para outro no
+
+    public T getDado() {
+        return dado;
+    }
+
+    public void setDado(T dado) {
+        this.dado = dado;
+    }
+
+    public No<T> getProximo() {
+        return proximo;
+    }
+
+    public void setProximo(No<T> proximo) {
+        this.proximo = proximo;
+    }
+
+    @Override
+    public String toString() {
+        return this.dado.toString();
+    }
+}
