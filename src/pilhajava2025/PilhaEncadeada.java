e5ed6c5da5931cd7dbe317aeaaa87829ccd8b05b
José Carlos Souza <166337638+jose1souza@users.noreply.github.com>
2025-05-08 10:15:13 -0300

    Add files via upload

---
+++ src/pilhajava2025/PilhaEncadeada.java
@@ -0,0 +1,46 @@
+package pilhajava2025;
+
+public class PilhaEncadeada<T> {
+    private No<T> topo;
+    private int qtd;
+    
+    public PilhaEncadeada(){
+        this.topo = null;
+        this.qtd=0;
+    }
+    
+    public boolean push(T dado){
+        No<T> novoNo = new No();
+        if(novoNo==null) // não conseguiu alocar
+            return false;
+        novoNo.setDado(dado);
+        novoNo.setProximo(topo);
+        this.topo = novoNo;
+        this.qtd++;
+        return true;
+    }
+    
+    public boolean isEmpty(){
+        return this.topo == null;
+    }
+    
+    public T pop(){
+        if(!isEmpty()){
+        T valor = this.topo.getDado();
+        this.topo = this.topo.getProximo();
+        return valor;
+        }
+        return null;
+    }
+    
+    @Override
+    public String toString(){
+        
+        StringBuilder retorno =
+                new StringBuilder("Topo\n");
+        for(int i = this.topo ; i>=0; i--)
+            retorno.append(this.elementos[i]+"\n");
+        
+        return retorno.toString();
+    }
+}
---
+++ src/pilhajava2025/PilhaJava2025.java
@@ -0,0 +1,83 @@
+package pilhajava2025;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+
+public class PilhaJava2025 {
+    static Scanner leia = new Scanner(System.in);
+
+    private static int menu() {
+        System.out.println("\n--- Menu Pilha ---");
+        System.out.println("1. Inserir elemento");
+        System.out.println("2. Remover elemento");
+        System.out.println("3. Mostrar topo");
+        System.out.println("4. Mostrar pilha");
+        System.out.println("0. Sair");
+        System.out.printf("Digite a opção desejada:" );
+    	return leia.nextInt();
+	}
+    
+    public static void lerRecorde(Recorde recorde) {
+          Scanner scanner = new Scanner(System.in);
+          System.out.println("Digite o nome do atleta: ");
+          recorde.setNome(scanner.nextLine());
+          System.out.println("Digite a data do recorde (dd/MM/yyyy): ");
+          String dataStr = scanner.nextLine();
+          LocalDate dataFormatada = LocalDate.parse(dataStr,DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+          recorde.setDataRecorde(dataFormatada) ;
+          System.out.println("Digite o tempo do recorde (em segundos): ");
+          recorde.setTempo(scanner.nextDouble());
+    }
+
+
+
+    public static void main(String[] args) {
+        
+        int opcao, dado;
+        System.out.println("Tamanho da pilha:");
+        int tamanho = leia.nextInt();
+        Pilha<Recorde> minhaPilha = new Pilha(tamanho);
+        
+        do{
+            opcao = menu();
+            switch(opcao){
+                case 1: Recorde novoRecorde = new Recorde();
+                        System.out.println("Dado:");
+                        //ler dados do novoRecorde
+                        lerRecorde(novoRecorde);
+                        if(!minhaPilha.isEmpty()){
+                            if(novoRecorde.getTempo() < 
+                                    minhaPilha.peek().getTempo())
+                                minhaPilha.push(novoRecorde);
+                        }
+                            else{
+                                boolean inseriu = minhaPilha.push(novoRecorde);
+                                if(!inseriu)
+                                    System.out.println("Fila cheia");
+                            }
+                        
+                break;
+                case 2: if(!minhaPilha.isEmpty())
+                            System.out.println("Topo removido:"
+                                    +minhaPilha.pop());
+                        else
+                            System.out.println("Pilha Vazia!");
+                break;
+                case 3: if(!minhaPilha.isEmpty())
+                            System.out.println("Topo:"
+                                    +minhaPilha.peek());
+                        else
+                            System.out.println("Pilha Vazia!");
+                break;
+                case 4: System.out.println(minhaPilha);
+                break;
+                case 0: System.out.println("Saindo");
+                break;
+            }// fim swtich
+            leia.nextLine();// pausa
+        }while(opcao!=0);
+       
+    }
+    
+}
